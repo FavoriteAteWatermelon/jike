32356f790fb5974e4698b3b19290bb8e5e83199a
shun <1039849632@qq.com>
2020-06-06 00:20:46 -0700

    jike

--- jike/SceneDelegate.m
+++ jike/SceneDelegate.m
@@ -7,8 +7,8 @@
 //
 
 #import "SceneDelegate.h"
-
-@interface SceneDelegate ()
+#import "ViewController.h"
+@interface SceneDelegate ()<UITabBarControllerDelegate>
 
 @end
 
@@ -16,11 +16,45 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
+    UITabBarController  *tabbarController = [[UITabBarController alloc] init];
+//    UIViewController *controller1 = [[UIViewController alloc]init];
+    ViewController *view = [[ViewController alloc]init];
+
+    
+//    controller1.view.backgroundColor = [UIColor redColor];
+    view.tabBarItem.title = @"新闻";
+    view.tabBarItem.image = [UIImage imageNamed:@"home.png"];
+    view.tabBarItem.selectedImage = [UIImage imageNamed:@"home_selected.png"];
+    UIViewController *controller2 = [[UIViewController alloc]init];
+    controller2.view.backgroundColor = [UIColor yellowColor];
+    controller2.tabBarItem.title =@"视频";
+    controller2.tabBarItem.image = [UIImage imageNamed:@"video.png"];
+    controller2.tabBarItem.selectedImage = [UIImage imageNamed:@"video_selected.png"];
+    UIViewController *controller3 = [[UIViewController alloc]init];
+    controller3.view.backgroundColor = [UIColor greenColor];
+    controller3.tabBarItem.image = [UIImage imageNamed:@"recommend.png"];
+    controller3.tabBarItem.selectedImage = [UIImage imageNamed:@"recommend_selected.png"];
+    controller3.tabBarItem.title = @"推荐";
+    UIViewController *controller4 = [[UIViewController alloc]init];
+    controller4.view.backgroundColor = [UIColor blueColor];
+    controller4.tabBarItem.title = @"我的";
+    controller4.tabBarItem.image = [UIImage imageNamed:@"me.png"];
+    controller4.tabBarItem.selectedImage = [UIImage imageNamed:@"me_selected.png"];
+        UINavigationController *nv = [[UINavigationController alloc] initWithRootViewController:tabbarController];
+    [tabbarController setViewControllers:@[view,controller2,controller3,controller4]];
+//    代理设置为当前的class
+    tabbarController.delegate = self;
+    self.window.rootViewController = nv;
+    [self.window makeKeyAndVisible];
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 }
 
+//- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController{
+//    NSLog(@"did loading");
+//    
+//}
 
 - (void)sceneDidDisconnect:(UIScene *)scene {
     // Called as the scene is being released by the system.
--- jike/ViewController.m
+++ jike/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource>
 
 @end
 
@@ -16,8 +16,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    UITableView *tableView = [[UITableView alloc]initWithFrame:self.view.bounds];
+    
+    tableView.dataSource = self;
+    [self.view addSubview:tableView];
+    
+
+
+//    UIView *view = [[UIView alloc] init];
+//    view.backgroundColor = [UIColor greenColor];
+//    view.frame=CGRectMake(100, 100, 100, 100);
+//    self.view.backgroundColor = [UIColor whiteColor];
+//    [self.view addSubview:view];
+////    初始化点击事件
+//    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pushController)];
+////    对应view增加点击事件
+//    [view addGestureRecognizer:tapGesture];
+//    // Do any additional setup after loading the view.
+//}
+////对应事件
+//- (void) pushController{
+//
+//    UIViewController *viewController = [[UIViewController alloc]init];
+//    viewController.view.backgroundColor = [UIColor whiteColor];
+//    viewController.navigationItem.title = @"内容";
+////    设置右侧button
+//    viewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"右侧button" style:UIBarButtonItemStylePlain target:self action:nil];
+////    执行push事件
+//    [self.navigationController pushViewController:viewController animated:YES];
+    
+
+    
 }
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 20;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+//    复用高效实现复用列表
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"id"];
+//    回收
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"id"];
+    }
+    cell.textLabel.text = @"主标题";
+    cell.detailTextLabel.text = @"副标题";
+    cell.imageView.image = [UIImage imageNamed:@"video_selected.png"];
+    return cell;
+}
+
 
 
 @end
